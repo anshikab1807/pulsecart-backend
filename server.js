e65b25cd8c28ffbf81ada1ff7c7fd67a9d172115
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-11-11 23:41:28 +0530

    backend

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use('/api/razorpay', razorpayRoutes);
 
 // For local development
 if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5001;
   app.listen(PORT, () => {
     console.log(`✅ Server running at http://localhost:${PORT}`);
   });
